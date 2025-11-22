9dc81cfbf6494f0f490e76eea066bf45e96858b2
takaoksuzuki <52984304+takaoksuzuki@users.noreply.github.com>
2025-11-22 11:11:18 +0900

    Update run_bayestraits.sh

--- src/SHELL/run_bayestraits.sh
+++ src/SHELL/run_bayestraits.sh
@@ -1,7 +1,8 @@
-#PBS -S /bin/bash
-#PBS -l ncpus=1
-#PBS -m abe
+#!/bin/bash
 
-cd /YOURPATH/20230504_GTDB207_cellshape_motility_sporulation_ALL/
-
-./BayesTraitsV4 Proteobacteria_cellshape_motility_sporulation_bac120_r207.tree ch_GTDB207_cellshape_motility_sporulation_Proteobacteria.tsv < bac_1_r207_Proteobacteria_cellshape_motility_sporulation_inputcommands_Res_g0101_a.txt > fout_a_cellshape_motility_sporulation_Proteobacteria_rj_g0101.txt
+# Run BayesTraits for one phylum
+./BayesTraits/BayesTraitsV4 \
+  data/treefile.tree \
+  data/traitfile.tsv \
+  < results/inputcommands_Proteobacteria.txt \
+  > results/output_Proteobacteria.txt
